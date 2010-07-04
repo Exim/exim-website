52b33ebd755ffe58ffa039c47ba2538a9c6af6f3
Nigel Metheringham <nigel.metheringham@dev.intechnology.co.uk>
2010-07-04 19:27:35 +0100

    Reformat js

--- templates/web/doc/index.js
+++ templates/web/doc/index.js
@@ -1,10 +1,11 @@
 // Add the expand/collapse functionality
-	$('#chapters')
-		.addClass('expandable')
-		.find('.button')
-		.click(function(){
-			$(this).parent().toggleClass('open');
-		});
-	$('#options img.expand').click(function(){ $('.chapter').addClass('open'); });
-	$('#options img.collapse').click(function(){ $('.chapter').removeClass('open') });
-	$('#options').removeClass('hidden');
+$('#chapters').addClass('expandable').find('.button').click(function () {
+    $(this).parent().toggleClass('open');
+});
+$('#options img.expand').click(function () {
+    $('.chapter').addClass('open');
+});
+$('#options img.collapse').click(function () {
+    $('.chapter').removeClass('open')
+});
+$('#options').removeClass('hidden');
