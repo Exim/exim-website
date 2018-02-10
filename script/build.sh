9d0f50e12e32b7cf6732e7f2924f3ce290933e43
Heiko Schlittermann (HS12-RIPE) <hs@schlittermann.de>
2018-02-10 22:42:44 +0100

    Remove file name extensions from build scripts
    For compatibilty maintain the old names by symlinks

--- script/build.sh
+++ script/build.sh
@@ -1,13 +1,1 @@
-#!/bin/sh
-
-TARGET=${1-/srv/www/vhosts/www.exim.org}
-LATEST=`(cd docbook; ls -1d [0-9].[0-9]* | tail -1)`
-
-set -x
-script/gen.pl \
-  --web \
-  --spec docbook/*/spec.xml \
-  --filter  docbook/*/filter.xml \
-  --latest ${LATEST} \
-  --tmpl templates \
-  --docroot ${TARGET}
+build
