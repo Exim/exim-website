Exim/exim-website

commit 8b033016be5cea59ef168e8b574f233445b214c6

Improve readability of the startpage

templates/web/index.xsl
@@ -47,7 +47,7 @@
          </p>
 
          <p id="version_info">
-            <xsl:text>The current version is </xsl:text> <xsl:value-of select="/content/current_version"/><xsl:text>.</xsl:text>
+            <xsl:text>The current version is </xsl:text> <xsl:value-of select="/content/current_version"/><xsl:text>.</xsl:text> </br>
 	    <xsl:text>If necessary, we publish maintenance releases. These releases are mainly intended for package maintainers.</xsl:text>
 
 	    <!--

commit 6688df4e60498026b5d343448031804e347b957e

fix: broken index.xsl
broken by 8b033016be5cea59ef168e8b574f233445b214c6

templates/web/index.xsl
@@ -47,7 +47,7 @@
          </p>
 
          <p id="version_info">
-            <xsl:text>The current version is </xsl:text> <xsl:value-of select="/content/current_version"/><xsl:text>.</xsl:text> </br>
+            <xsl:text>The current version is </xsl:text> <xsl:value-of select="/content/current_version"/><xsl:text>.</xsl:text> <br/>
 	    <xsl:text>If necessary, we publish maintenance releases. These releases are mainly intended for package maintainers.</xsl:text>
 
 	    <!--